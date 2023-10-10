491d61899c5cdf5413ab38688128ecb395971058
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2023-10-10 20:52:07 +0300

    first

---
+++ src/main/java/edu/hw1/task_0.java
@@ -0,0 +1,14 @@
+package edu.hw1;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class task_0 {
+    private final static Logger LOGGER = LogManager.getLogger();
+
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        LOGGER.info("Привет мир!");
+    }
+}
---
+++ src/test/java/edu/hw1/task_0Test.java
@@ -0,0 +1,7 @@
+package edu.hw1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class task_0Test {
+
+}
