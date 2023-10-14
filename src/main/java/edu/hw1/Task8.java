92fa48988224ef502ada2fc46cd9bc5422f4eeb9
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2023-10-14 18:14:22 +0300

    task_0_1_2_3_4_5_6_7_8

---
+++ src/main/java/edu/hw1/Task8.java
@@ -0,0 +1,77 @@
+package edu.hw1;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Task8 {
+    private static final int EIGHT = 8;
+
+    private Task8() {
+    }
+
+    public static boolean checkLeft(int[][] desk, int i, int j) {
+        boolean result = false;
+        if (i - 1 >= 0 && j - 2 >= 0 && desk[i - 1][j - 2] == 1 ||
+            i + 1 < EIGHT && j - 2 >= 0 && desk[i + 1][j - 2] == 1) {
+            result = true;
+        }
+        return result;
+    }
+
+    public static boolean checkRight(int[][] desk, int i, int j) {
+        boolean result = false;
+        if (i - 1 >= 0 && j + 2 < EIGHT && desk[i - 1][j + 2] == 1 ||
+            i + 1 < EIGHT && j + 2 < EIGHT && desk[i + 1][j + 2] == 1) {
+            result = true;
+        }
+        return result;
+    }
+
+    public static boolean checkAbove(int[][] desk, int i, int j) {
+        boolean result = false;
+        if (i - 2 >= 0 && j - 1 >= 0 && desk[i - 2][j - 1] == 1 ||
+            i - 2 >= 0 && j + 1 < EIGHT && desk[i - 2][j + 1] == 1) {
+            result = true;
+        }
+        return result;
+    }
+
+    public static boolean checkUnder(int[][] desk, int i, int j) {
+        boolean result = false;
+        if (i + 2 < EIGHT && j - 1 >= 0 && desk[i + 2][j - 1] == 1 ||
+            i + 2 < EIGHT && j + 1 < EIGHT && desk[i + 2][j + 1] == 1) {
+            result = true;
+        }
+        return result;
+    }
+
+    public static boolean knightBoardCapture(int[][] desk) {
+        boolean result = true;
+        for (int i = 0; i < EIGHT && result; ++i) {
+            for (int j = 0; j < EIGHT && result; ++j) {
+                if (desk[i][j] == 1) {
+                    if (checkLeft(desk, i, j) || checkRight(desk, i, j) || checkAbove(desk, i, j) ||
+                        checkUnder(desk, i, j)) {
+                        result = false;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    private final static Logger LOGGER = LogManager.getLogger();
+
+    //main
+    public static void main(String[] args) {
+        int[][] desk = {{0, 0, 0, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 1, 0, 0, 0}};
+        LOGGER.info(knightBoardCapture(desk));
+    }
+}
---
+++ src/test/java/edu/hw1/Task8Test.java
@@ -0,0 +1,57 @@
+package edu.hw1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Task8Test {
+
+    @Test
+    @DisplayName("test1")
+    void knightBoardCapture1() {
+        int[][] desk = {{0, 0, 0, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 1, 0, 0, 0}};
+        boolean actual = Task8.knightBoardCapture(desk);
+        boolean expected = true;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("test2")
+    void knightBoardCapture2() {
+        int[][] desk = {{1, 0, 1, 0, 1, 0, 1, 0},
+            {0, 1, 0, 1, 0, 1, 0, 1},
+            {0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 1, 0, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 0, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 1, 0, 1, 0, 1}};
+        boolean actual = Task8.knightBoardCapture(desk);
+        boolean expected = false;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("test3")
+    void knightBoardCapture3() {
+        int[][] desk = {{0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0}};
+        boolean actual = Task8.knightBoardCapture(desk);
+        boolean expected = false;
+        assertEquals(expected, actual);
+    }
+}
