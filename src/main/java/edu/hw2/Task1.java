e21cdf43bb1e818a1fed3fe77a16a92906be0822
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2023-10-18 21:58:09 +0300

    task_1

--- src/main/java/edu/hw2/Task1.java
+++ src/main/java/edu/hw2/Task1.java
@@ -6,7 +6,23 @@ import org.apache.logging.log4j.Logger;
 @SuppressWarnings("uncommentedmain")
 
 public class Task1 {
+    private static final double FOUR = 4;
+
     private Task1() {
+    }
+
+    private final static Logger LOGGER = LogManager.getLogger();
+
+    //main
+    public static void main(String[] args) {
+        var two = new Expr.Constant(2);
+        var four = new Expr.Constant(FOUR);
+        var negOne = new Expr.Negate(new Expr.Constant(1));
+        var sumTwoFour = new Expr.Addition(two, four);
+        var mult = new Expr.Multiplication(sumTwoFour, negOne);
+        var exp = new Expr.Exponent(mult, 2);
+        var res = new Expr.Addition(exp, new Expr.Constant(1));
+        LOGGER.info(res + " = " + res.evaluate());
     }
 
     public sealed interface Expr {
@@ -49,19 +65,5 @@ public class Task1 {
                 }
             }
         }
-    }
-
-    private final static Logger LOGGER = LogManager.getLogger();
-
-    //main
-    public static void main(String[] args) {
-        var two = new Expr.Constant(2);
-        var four = new Expr.Constant(4);
-        var negOne = new Expr.Negate(new Expr.Constant(1));
-        var sumTwoFour = new Expr.Addition(two, four);
-        var mult = new Expr.Multiplication(sumTwoFour, negOne);
-        var exp = new Expr.Exponent(mult, 2);
-        var res = new Expr.Addition(exp, new Expr.Constant(1));
-        LOGGER.info(res + " = " + res.evaluate());
     }
 }
