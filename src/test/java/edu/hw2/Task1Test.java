9b6f1a29e6264c700b2d67ebd5c53c518ca02b5e
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2023-10-19 21:33:37 +0300

    task_1_3

--- src/test/java/edu/hw2/Task1Test.java
+++ src/test/java/edu/hw2/Task1Test.java
@@ -2,127 +2,88 @@ package edu.hw2;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
-//import static org.junit.jupiter.params.*;
 
 public class Task1Test {
 
-    @Test
-    @DisplayName("Check Constant")
-    void checkConstantTest() {
-        double actual = new Task1.Expr.Constant(1).evaluate();
-        double expected = 1;
-        assertEquals(expected, actual);
+    @ParameterizedTest(name = "Check Constant")
+    @CsvSource({
+        "1, 1",
+        "-1, -1",
+        "0, 0"
+    })
+    void checkConstantTest(double actual, double expected) {
+        assertEquals(expected, new Task1.Expr.Constant(actual).evaluate());
+    }
 
-        actual = new Task1.Expr.Constant(-1).evaluate();
-        expected = -1;
-        assertEquals(expected, actual);
+    @ParameterizedTest(name = "Check Negate")
+    @CsvSource({
+        "1, -1",
+        "-1, 1",
+        "0, 0"
+    })
+    void checkNegateTest(double actual, double expected) {
+        assertEquals(expected, new Task1.Expr.Negate(new Task1.Expr.Constant(actual)).evaluate());
+    }
 
-        actual = new Task1.Expr.Constant(0).evaluate();
-        expected = 0;
+    @ParameterizedTest(name = "Check Exponent")
+    @CsvSource({
+        "1, 2, 1",
+        "1, 0, 1",
+        "-1, 2, 1",
+        "-1, 3, -1",
+        "-1, 0, 1",
+        "0, 0, 1",
+        "0, 2, 0"
+    })
+    void checkExponentTest(double valueLeft, int degree, double expected) {
+        double actual = new Task1.Expr.Exponent(new Task1.Expr.Constant(valueLeft), degree).evaluate();
         assertEquals(expected, actual);
     }
 
-    @Test
-    @DisplayName("Check Negate")
-    void checkNegateTest() {
-        double actual = new Task1.Expr.Negate(new Task1.Expr.Constant(1)).evaluate();
-        double expected = -1;
-        assertEquals(expected, actual);
-
-        actual = new Task1.Expr.Negate(new Task1.Expr.Constant(-1)).evaluate();
-        expected = 1;
-        assertEquals(expected, actual);
-
-        actual = new Task1.Expr.Negate(new Task1.Expr.Constant(0)).evaluate();
-        expected = 0;
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    @DisplayName("Check Exponent")
-    void checkExponentTest() {
-        double actual = new Task1.Expr.Exponent(new Task1.Expr.Constant(1), 2).evaluate();
-        double expected = 1;
-        assertEquals(expected, actual);
-
-        actual = new Task1.Expr.Exponent(new Task1.Expr.Constant(1), 0).evaluate();
-        assertEquals(expected, actual);
-
-        actual = new Task1.Expr.Exponent(new Task1.Expr.Constant(-1), 2).evaluate();
-        assertEquals(expected, actual);
-
-        actual = new Task1.Expr.Exponent(new Task1.Expr.Constant(-1), 3).evaluate();
-        expected = -1;
-        assertEquals(expected, actual);
-
-        actual = new Task1.Expr.Exponent(new Task1.Expr.Constant(-1), 0).evaluate();
-        expected = 1;
-        assertEquals(expected, actual);
-
-        actual = new Task1.Expr.Exponent(new Task1.Expr.Constant(0), 0).evaluate();
-        assertEquals(expected, actual);
-
-        actual = new Task1.Expr.Exponent(new Task1.Expr.Constant(0), 2).evaluate();
-        expected = 0;
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    @DisplayName("Check Addition")
-    void checkAdditionTest() {
-        double actual = new Task1.Expr.Addition(new Task1.Expr.Constant(1), new Task1.Expr.Constant(2)).evaluate();
-        double expected = 3;
-        assertEquals(expected, actual);
-
-        actual = new Task1.Expr.Addition(new Task1.Expr.Constant(-1), new Task1.Expr.Constant(1)).evaluate();
-        expected = 0;
-        assertEquals(expected, actual);
-
-        actual = new Task1.Expr.Addition(new Task1.Expr.Constant(0), new Task1.Expr.Constant(0)).evaluate();
-        assertEquals(expected, actual);
-
-        actual = new Task1.Expr.Addition(new Task1.Expr.Constant(-2), new Task1.Expr.Constant(1)).evaluate();
-        expected = -1;
-        assertEquals(expected, actual);
-
-        actual = new Task1.Expr.Addition(new Task1.Expr.Constant(0.5), new Task1.Expr.Constant(0.9)).evaluate();
-        expected = 1.4;
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    @DisplayName("Check Multiplication")
-    void checkMultiplicationTest() {
+    @ParameterizedTest(name = "Check Addition")
+    @CsvSource({
+        "1, 2, 3",
+        "1, -1, 0",
+        "0, 0, 0",
+        "-2, 1, -1",
+        "0.5, 0.9, 1.4"
+    })
+    void checkAdditionTest(double valueLeft, double valueRight, double expected) {
         double actual =
-            new Task1.Expr.Multiplication(new Task1.Expr.Constant(1), new Task1.Expr.Constant(2)).evaluate();
-        double expected = 2;
+            new Task1.Expr.Addition(new Task1.Expr.Constant(valueLeft), new Task1.Expr.Constant(valueRight)).evaluate();
         assertEquals(expected, actual);
+    }
 
-        actual = new Task1.Expr.Multiplication(new Task1.Expr.Constant(1), new Task1.Expr.Constant(0)).evaluate();
-        expected = 0;
-        assertEquals(expected, actual);
-
-        actual = new Task1.Expr.Multiplication(new Task1.Expr.Constant(1), new Task1.Expr.Constant(-1)).evaluate();
-        expected = -1;
-        assertEquals(expected, actual);
-
-        actual = new Task1.Expr.Multiplication(new Task1.Expr.Constant(-1), new Task1.Expr.Constant(0)).evaluate();
-        expected = 0;
+    @ParameterizedTest(name = "Check Multiplication")
+    @CsvSource({
+        "1, 2, 2",
+        "1, 0, 0",
+        "1, -1, -1",
+        "-1, 0, 0"
+    })
+    void checkMultiplicationTest(double valueLeft, double valueRight, double expected) {
+        double actual = new Task1.Expr.Multiplication(
+            new Task1.Expr.Constant(valueLeft),
+            new Task1.Expr.Constant(valueRight)
+        ).evaluate();
         assertEquals(expected, actual);
     }
 
     @Test
     @DisplayName("MultiCheck")
     void multiCheckTest() {
-        var actual = new Task1.Expr.Addition(
-            new Task1.Expr.Constant(1),
+        Task1.Expr actual = new Task1.Expr.Addition(
             new Task1.Expr.Exponent(new Task1.Expr.Multiplication(new Task1.Expr.Addition(
                 new Task1.Expr.Constant(2),
                 new Task1.Expr.Constant(4)
-            ), new Task1.Expr.Negate(new Task1.Expr.Constant(1))), 2)
+            ), new Task1.Expr.Negate(new Task1.Expr.Constant(1))), 2),
+            new Task1.Expr.Constant(1)
         );
         double expected = 37;
+        assertEquals(expected, actual.evaluate());
     }
 }
