10c70a977645874f73558d9abe16892fabf05468
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2023-10-21 15:12:29 +0300

    task_1_3

---
+++ src/main/java/edu/hw2/Task4.java
@@ -0,0 +1,33 @@
+package edu.hw2;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@SuppressWarnings("uncommentedmain")
+
+public class Task4 {
+    private Task4() {
+    }
+
+    private final static Logger LOGGER = LogManager.getLogger();
+
+    public static void main(String[] args) {
+        LOGGER.info(callingInfo());
+    }
+
+    public static CallingInfo callingInfo()
+    {
+        String className;
+        String methodName;
+        try {
+            throw new Exception();
+        } catch (Throwable exception) {
+            StackTraceElement[] ste = exception.getStackTrace();
+            className = ste[1].getClassName();
+            methodName = ste[1].getMethodName();
+        }
+        return new CallingInfo(className, methodName);
+    }
+    public record CallingInfo(String className, String methodName) {
+    }
+}
