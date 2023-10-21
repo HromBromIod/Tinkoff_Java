2787027673d62aec19cbb953b490368ff7f731c0
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2023-10-21 19:30:05 +0300

    task_1_2_3_4

--- src/test/java/edu/hw2/Task4Test.java
+++ src/test/java/edu/hw2/Task4Test.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Task4Test {
     @Test
-    @DisplayName("Call checkThis1")
+    @DisplayName("Call from checkThis1")
     void checkThis1() {
         Task4.CallingInfo actual = Task4.callingInfo();
         Task4.CallingInfo expected = new Task4.CallingInfo("edu.hw2.Task4Test", "checkThis1");
@@ -14,7 +14,7 @@ public class Task4Test {
     }
 
     @Test
-    @DisplayName("Call checkThis2")
+    @DisplayName("Call from person class from checkThis2")
     void checkThis2() {
         class CheckClass {
             CheckClass() {
