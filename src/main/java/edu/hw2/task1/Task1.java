67c22f297ad6f7b04e2558feec731346f66708f5
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2023-10-22 13:39:08 +0300

    project_1_hangman

---
+++ src/main/java/edu/hw2/task1/Task1.java
@@ -0,0 +1,27 @@
+package edu.hw2.task1;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@SuppressWarnings("uncommentedmain")
+
+public class Task1 {
+    private static final double FOUR = 4;
+
+    private Task1() {
+    }
+
+    private final static Logger LOGGER = LogManager.getLogger();
+
+    public static void main(String[] args) {
+        var two = new Expr.Constant(2);
+        var four = new Expr.Constant(FOUR);
+        var negOne = new Expr.Negate(new Expr.Constant(1));
+        var sumTwoFour = new Expr.Addition(two, four);
+        var mult = new Expr.Multiplication(sumTwoFour, negOne);
+        var exp = new Expr.Exponent(mult, 2);
+        var res = new Expr.Addition(exp, new Expr.Constant(1));
+
+        LOGGER.info(res + " = " + res.evaluate());
+    }
+}
--- src/test/java/edu/hw2/Task1Test.java
+++ src/test/java/edu/hw2/Task1Test.java
@@ -1,5 +1,6 @@
 package edu.hw2;
 
+import edu.hw2.task1.Expr;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -7,7 +8,6 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Task1Test {
-
     @ParameterizedTest(name = "Check Constant")
     @CsvSource({
         "1, 1",
@@ -15,7 +15,7 @@ public class Task1Test {
         "0, 0"
     })
     void checkConstantTest(double actual, double expected) {
-        assertEquals(expected, new Task1.Expr.Constant(actual).evaluate());
+        assertEquals(expected, new Expr.Constant(actual).evaluate());
     }
 
     @ParameterizedTest(name = "Check Negate")
@@ -25,7 +25,7 @@ public class Task1Test {
         "0, 0"
     })
     void checkNegateTest(double actual, double expected) {
-        assertEquals(expected, new Task1.Expr.Negate(new Task1.Expr.Constant(actual)).evaluate());
+        assertEquals(expected, new Expr.Negate(new Expr.Constant(actual)).evaluate());
     }
 
     @ParameterizedTest(name = "Check Exponent")
@@ -39,7 +39,7 @@ public class Task1Test {
         "0, 2, 0"
     })
     void checkExponentTest(double valueLeft, int degree, double expected) {
-        double actual = new Task1.Expr.Exponent(new Task1.Expr.Constant(valueLeft), degree).evaluate();
+        double actual = new Expr.Exponent(new Expr.Constant(valueLeft), degree).evaluate();
         assertEquals(expected, actual);
     }
 
@@ -53,7 +53,7 @@ public class Task1Test {
     })
     void checkAdditionTest(double valueLeft, double valueRight, double expected) {
         double actual =
-            new Task1.Expr.Addition(new Task1.Expr.Constant(valueLeft), new Task1.Expr.Constant(valueRight)).evaluate();
+            new Expr.Addition(new Expr.Constant(valueLeft), new Expr.Constant(valueRight)).evaluate();
         assertEquals(expected, actual);
     }
 
@@ -65,24 +65,40 @@ public class Task1Test {
         "-1, 0, 0"
     })
     void checkMultiplicationTest(double valueLeft, double valueRight, double expected) {
-        double actual = new Task1.Expr.Multiplication(
-            new Task1.Expr.Constant(valueLeft),
-            new Task1.Expr.Constant(valueRight)
+        double actual = new Expr.Multiplication(
+            new Expr.Constant(valueLeft),
+            new Expr.Constant(valueRight)
         ).evaluate();
         assertEquals(expected, actual);
     }
 
     @Test
-    @DisplayName("MultiCheck")
-    void multiCheckTest() {
-        Task1.Expr actual = new Task1.Expr.Addition(
-            new Task1.Expr.Exponent(new Task1.Expr.Multiplication(new Task1.Expr.Addition(
-                new Task1.Expr.Constant(2),
-                new Task1.Expr.Constant(4)
-            ), new Task1.Expr.Negate(new Task1.Expr.Constant(1))), 2),
-            new Task1.Expr.Constant(1)
+    @DisplayName("MultiCheck1")
+    void multiCheckTest1() {
+        Expr actual = new Expr.Addition(
+            new Expr.Exponent(new Expr.Multiplication(new Expr.Addition(
+                new Expr.Constant(2),
+                new Expr.Constant(4)
+            ), new Expr.Negate(new Expr.Constant(1))), 2),
+            new Expr.Constant(1)
         );
         double expected = 37;
+        assertEquals(expected, actual.evaluate());
+    }
+
+    @Test
+    @DisplayName("MultiCheck2")
+    void multiCheckTest2() {
+        var three = new Expr.Constant(3);
+        var five = new Expr.Constant(5);
+        var eight = new Expr.Constant(8);
+        var negTwo = new Expr.Negate(new Expr.Constant(2));
+        var sumThreeFive = new Expr.Addition(three, five);
+        var multFirst = new Expr.Multiplication(sumThreeFive, negTwo);
+        var multSecond = new Expr.Addition(multFirst, eight);
+        var exp = new Expr.Exponent(multSecond, 3);
+        var actual = new Expr.Addition(exp, new Expr.Constant(1));
+        double expected = -511;
         assertEquals(expected, actual.evaluate());
     }
 }
