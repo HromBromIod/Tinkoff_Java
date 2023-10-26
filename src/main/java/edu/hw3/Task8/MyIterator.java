a0b935338eb76929867662e642dffc8f905a1eee
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2023-10-26 23:46:02 +0300

    task_1_2_3_4_5_6_7

---
+++ src/main/java/edu/hw3/Task8/MyIterator.java
@@ -0,0 +1,18 @@
+package edu.hw3.Task8;
+
+import org.jetbrains.annotations.NotNull;
+import java.util.Iterator;
+
+public class MyIterator implements Iterable<Object> {
+
+    @NotNull
+    @Override
+    public Iterator<Object> iterator() {
+        return null;
+    }
+
+    @Override
+    public void add(Object obj) {
+
+    }
+}
