c12c2748e6a1c9036c492c1522199e7ed8f93da9
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2023-11-03 21:02:57 +0300

    task_1_2

--- src/main/java/edu/hw3/Task2/Clusterizator3000.java
+++ src/main/java/edu/hw3/Task2/Clusterizator3000.java
@@ -1,21 +1,22 @@
 package edu.hw3.Task2;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 public class Clusterizator3000 {
     private Clusterizator3000() {
     }
 
-    public static ArrayList<String> clusterize(String string) {
-        ArrayList<String> result = new ArrayList<>();
+    public static List<String> clusterize(String string) {
+        List<String> result = new ArrayList<>();
         for (int i = 0; i < string.length(); ++i) {
             if (string.charAt(i) != '(' && string.charAt(i) != ')') {
                 return result;
             }
         }
         String cut = string;
-        for (int i = 1; !cut.equalsIgnoreCase(""); ++i) {
+        while (!cut.isEmpty()) {
             String str = cutBalance(cut);
             if (str.equalsIgnoreCase("")) {
                 cut = str;
@@ -31,15 +32,14 @@ public class Clusterizator3000 {
         String result = "";
         Stack<Character> st = new Stack<>();
         st.push(string.charAt(0));
-        int iterator = 1;
-        while (!st.isEmpty() && iterator < string.length()) {
+        int iterator;
+        for (iterator = 1; !st.isEmpty() && iterator < string.length(); ++iterator) {
             char c = string.charAt(iterator);
             if (c == ')' && c != st.peek()) {
                 st.pop();
             } else {
                 st.push(c);
             }
-            iterator += 1;
         }
         if (st.isEmpty() && iterator <= string.length()) {
             result = string.substring(0, iterator);
