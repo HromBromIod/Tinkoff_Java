4a29ee49f824fcb3a61ea05d8cca7b888d2e319c
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2023-11-13 12:18:02 +0300

    task_4

---
+++ src/main/java/edu/hw5/Task4.java
@@ -0,0 +1,22 @@
+package edu.hw5;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Task4 {
+    private Task4() {
+    }
+
+    public static final Pattern PASSWORD = Pattern.compile("[~!@#$%^&*|]");
+
+    public static boolean isCorrectPassword(String password) {
+        boolean result = false;
+        if (password != null) {
+            Matcher matcher = PASSWORD.matcher(password);
+            if (matcher.find()) {
+                result = true;
+            }
+        }
+        return result;
+    }
+}
