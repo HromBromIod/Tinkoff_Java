ea55a73079f84f7eab80f5bef4371cbd9c4556de
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2023-11-13 18:17:08 +0300

    task_1_4_5_6_7

---
+++ src/main/java/edu/hw5/Task1.java
@@ -0,0 +1,56 @@
+package edu.hw5;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Task1 {
+    private Task1() {
+    }
+
+    private final static int START_DATE_OF_SESSION = 1;
+    private final static int START_TIME_OF_SESSION = 4;
+    private final static int END_DATE_OF_SESSION = 6;
+    private final static int END_TIME_OF_SESSION = 9;
+    public static final Pattern DATA_PATTERN = Pattern.compile(
+        "^(\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2]\\d|3[0-1])),"
+            + " (([0-1]\\d|2[0-4]):[0-5]\\d) - (\\d{4}-(0[1-9]|1[0-2])"
+            + "-(0[1-9]|[1-2]\\d|3[0-1])), (([0-1]\\d|2[0-4]):[0-5]\\d)$");
+
+    public static Duration averageSessionTime(List<String> baseOfDats) throws RuntimeException {
+        int sessionsCount = 0;
+        int fullDuration = 0;
+        if (!baseOfDats.isEmpty() && baseOfDats != null) {
+            for (String session : baseOfDats) {
+                Matcher sessionMatcher = DATA_PATTERN.matcher(session);
+                if (sessionMatcher.find()) {
+                    try {
+                        LocalDateTime startOfSession = LocalDateTime.of(
+                            LocalDate.parse(sessionMatcher.group(START_DATE_OF_SESSION)),
+                            LocalTime.parse(sessionMatcher.group(START_TIME_OF_SESSION))
+                        );
+                        LocalDateTime endOfSession = LocalDateTime.of(
+                            LocalDate.parse(sessionMatcher.group(END_DATE_OF_SESSION)),
+                            LocalTime.parse(sessionMatcher.group(END_TIME_OF_SESSION))
+                        );
+                        fullDuration +=
+                            (int) Duration.between(startOfSession, endOfSession).getSeconds();
+                        sessionsCount += 1;
+                    } catch (DateTimeParseException exception) {
+                        throw new RuntimeException("Parse error!");
+                    }
+                } else {
+                    throw new RuntimeException("Incorrect data format!");
+                }
+            }
+        } else {
+            throw new RuntimeException("Incorrect base of dats!");
+        }
+        return Duration.ofSeconds((long) fullDuration / sessionsCount);
+    }
+}
---
+++ src/test/java/edu/hw5/Task1Test.java
@@ -0,0 +1,46 @@
+package edu.hw5;
+
+import static edu.hw5.Task1.averageSessionTime;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.time.Duration;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class Task1Test {
+    private Task1Test() {
+    }
+
+    @Test
+    @DisplayName("null list test")
+    void nullListTest() {
+        assertThrows(RuntimeException.class, () -> averageSessionTime(null));
+    }
+
+    @Test
+    @DisplayName("empty list test")
+    void emptyListTest() {
+        assertThrows(RuntimeException.class, () -> averageSessionTime(List.of()));
+    }
+
+    @Test
+    @DisplayName("invalid dats in list test")
+    void invalidDatsInListTest() {
+        assertThrows(RuntimeException.class, () -> averageSessionTime(List.of(
+            "2022-03-12, 20:20 - 2022-43-12, 23:50",
+            "2022-04-01, 21:30 - 2022-04-02, 01:20"
+        )));
+    }
+
+    @Test
+    @DisplayName("valid list test")
+    void validDatsInListTest() {
+        String expected = "PT3H40M";
+        String actual = averageSessionTime(List.of(
+            "2022-03-12, 20:20 - 2022-03-12, 23:50",
+            "2022-04-01, 21:30 - 2022-04-02, 01:20"
+        )).toString();
+        assertEquals(expected, actual);
+    }
+}
