d1b21ac09fa95a008916ff8990af2240e9a1472a
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2023-11-14 23:31:20 +0300

    task_1_2_3_4_5_6_7_8

--- src/main/java/edu/hw3/Task7/MyComparator.java
+++ src/main/java/edu/hw3/Task7/MyComparator.java
@@ -2,21 +2,6 @@ package edu.hw3.Task7;
 
 import java.util.Comparator;
 
-//@Override
-//public Comparator<Map.Entry<K, V>> getComparator() {
-// Adapt or create a key-based comparator
-//    if (tree.comparator != null) {
-//    return Map.Entry.comparingByKey(tree.comparator);
-//    }
-//    else {
-//    return (Comparator<Map.Entry<K, V>> & Serializable) (e1, e2) -> {
-@SuppressWarnings("unchecked")
-//                    Comparable<? super K> k1 = (Comparable<? super K>) e1.getKey();
-//    return k1.compareTo(e2.getKey());
-//    };
-//    }
-//    }
-
 public class MyComparator implements Comparator<String> {
 
     @Override
