9fef602971c93feaf3a4438f0d35f6dd3ff69923
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2023-11-16 01:45:16 +0300

    task_1_20_not_final

--- src/main/java/edu/hw4/AnimalValidator.java
+++ src/main/java/edu/hw4/AnimalValidator.java
@@ -3,7 +3,6 @@ package edu.hw4;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Set;
 
 public class AnimalValidator {
     private AnimalValidator() {
--- src/main/java/edu/hw4/Tasks.java
+++ src/main/java/edu/hw4/Tasks.java
@@ -1,6 +1,11 @@
 package edu.hw4;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.stream.Collectors;
