f958c7eac40c554586049c48f795ce946f977c5e
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2023-11-19 18:50:49 +0300

    task_2_4

--- src/main/java/edu/hw6/Task4/Task4.java
+++ src/main/java/edu/hw6/Task4/Task4.java
@@ -10,16 +10,17 @@ import java.nio.file.Path;
 import java.util.zip.Adler32;
 import java.util.zip.CheckedOutputStream;
 
+@SuppressWarnings("uncommentedmain")
 public class Task4 {
     private Task4() {
     }
 
     public static void main(String[] args) {
-        try (
-            PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(new BufferedOutputStream(new CheckedOutputStream(
-                Files.newOutputStream(Path.of("src/main/java/edu/hw6/Task4/Brian Kernighan.txt")),
-                new Adler32()
-            )), StandardCharsets.UTF_8))) {
+        try (PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(
+            new BufferedOutputStream(new CheckedOutputStream(Files.newOutputStream(Path.of(
+                "src/main/java/edu/hw6/Task4/Brian Kernighan.txt")), new Adler32())),
+            StandardCharsets.UTF_8
+        ))) {
             printWriter.write("Programming is learned by writing programs. ― Brian Kernighan");
         } catch (IOException | NullPointerException e) {
             throw new RuntimeException(e);
