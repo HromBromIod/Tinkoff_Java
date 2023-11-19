424f4c2d12d54bcae2937fe3ca3a3b8253a2e494
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2023-11-19 23:59:31 +0300

    DateTimeFromFilter

--- src/main/java/edu/project3logAnalyzer/DateTimeFromFilter.java
+++ src/main/java/edu/project3logAnalyzer/DateTimeFromFilter.java
@@ -1,4 +1,17 @@
 package edu.project3logAnalyzer;
 
-public class DateTimeFromFilter {
+import java.time.LocalDateTime;
+import java.util.function.Predicate;
+
+public class DateTimeFromFilter implements Predicate<LocalDateTime> {
+    private final LocalDateTime from;
+
+    public DateTimeFromFilter(LocalDateTime from) {
+        this.from = from;
+    }
+
+    @Override
+    public boolean test(LocalDateTime localDateTime) {
+        return !this.from.isAfter(localDateTime);
+    }
 }
