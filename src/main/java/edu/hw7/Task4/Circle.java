04af04359f1d114033ee52b3250f210811d14ad6
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2023-11-26 23:18:19 +0300

    task_1_2_4

--- src/main/java/edu/hw7/Task4/Circle.java
+++ src/main/java/edu/hw7/Task4/Circle.java
@@ -3,6 +3,7 @@ package edu.hw7.Task4;
 public class Circle {
     private Circle() {
     }
+
     private static final Point CENTER = new Point(0.5, 0.5);
     private static final double RADIUS = 0.5;
 
--- src/main/java/edu/hw7/Task4/Task4.java
+++ src/main/java/edu/hw7/Task4/Task4.java
@@ -4,8 +4,36 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.LongAdder;
 import static edu.hw7.Task4.Circle.isPointInCircle;
 
+@SuppressWarnings({"RegexpSinglelineJava", "UncommentedMain", "MagicNumber"})
 public class Task4 {
     private Task4() {
+    }
+
+    //Среднее время ускорения решения (1_000_000_000): 1.8718092751097823
+    //One Thread PI: 3.141631592; Погрешность: 3.8938410206856844E-5
+    //Multi Thread PI: 3.1416058085664234; Погрешность: 1.3154976630236348E-5
+
+    //Среднее время ускорения решения (100_000_000): 1.547336522434407
+    //One Thread PI: 3.14171124; Погрешность: 1.1858641020667804E-4
+    //Multi Thread PI: 3.1414632656585306; Погрешность: 1.2938793126249237E-4
+
+    //Среднее время ускорения решения (10_000_000): 0.953596004543705
+    //One Thread PI: 3.1416276; Погрешность: 3.4946410206959655E-5
+    //Multi Thread PI: 3.1411734564693825; Погрешность: 4.191971204106082E-4
+
+    public static void main(String[] args) throws InterruptedException {
+        double expected;
+        var first = System.nanoTime();
+        expected = oneThreadPI(10_000_000);
+        System.out.printf("One Thread PI: %s%n", expected);
+        System.out.println(Math.abs(expected - Math.PI));
+        var second1 = System.nanoTime() - first;
+        first = System.nanoTime();
+        expected = fourThreadsPI(10_000_000);
+        System.out.printf("Multi Thread PI: %s%n", expected);
+        System.out.println(Math.abs(expected - Math.PI));
+        var second2 = System.nanoTime() - first;
+        System.out.println((double) second1 / second2);
     }
 
     public static double oneThreadPI(long countOfIterations) {
