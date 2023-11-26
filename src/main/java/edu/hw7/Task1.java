09f4aceda79d306fcb124ba17f97727e62a636f7
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2023-11-26 21:28:29 +0300

    task_1

---
+++ src/main/java/edu/hw7/Task1.java
@@ -0,0 +1,25 @@
+package edu.hw7;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Task1 {
+    private Task1() {
+    }
+
+    public static int parallelIncrement() throws InterruptedException {
+        AtomicInteger counter = new AtomicInteger(0);
+        Thread thread1 = new Thread(counter::incrementAndGet);
+        Thread thread2 = new Thread(counter::incrementAndGet);
+        Thread thread3 = new Thread(counter::incrementAndGet);
+        Thread thread4 = new Thread(counter::incrementAndGet);
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
+        thread1.join();
+        thread2.join();
+        thread3.join();
+        thread4.join();
+        return counter.get();
+    }
+}
---
+++ src/test/java/edu/hw7/Task1Test.java
@@ -0,0 +1,17 @@
+package edu.hw7;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import static edu.hw7.Task1.parallelIncrement;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Task1Test {
+    private Task1Test() {
+    }
+
+    @RepeatedTest(10)
+    @DisplayName("parallel counter increment test")
+    void test() throws InterruptedException {
+        assertEquals(4, parallelIncrement());
+    }
+}
