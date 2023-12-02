088cce8bfdbdb9069289bad5882f9c2ce160440f
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2023-12-02 23:54:27 +0300

    task_1_2_3

--- src/main/java/edu/hw5/Task3.java
+++ src/main/java/edu/hw5/Task3.java
@@ -9,7 +9,6 @@ public class Task3 {
     private Task3() {
     }
 
-    private static final String SPLITTER = ")|(";
     private static final String STRING_DATA_PATTERN_1 = "\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2]\\d|3[0-1])";
     private static final String STRING_DATA_PATTERN_2 = "\\d{4}-(0[1-9]|1[0-2])-([1-9]|[1-2]\\d|3[0-1])";
     private static final String STRING_DATA_PATTERN_3 = "([1-9]|[1-2]\\d|3[0-1])/([1-9]|1[0-2])/\\d{4}";
@@ -19,16 +18,16 @@ public class Task3 {
     private static final String STRING_DATA_PATTERN_7 = "tomorrow";
     private static final String STRING_DATA_PATTERN_8 = "(\\d+) days? ago";
     private static final String STRING_DATA_FULL_PATTERN =
-        "^((" + STRING_DATA_PATTERN_1 + SPLITTER + STRING_DATA_PATTERN_2 + SPLITTER + STRING_DATA_PATTERN_3 + SPLITTER
-            + STRING_DATA_PATTERN_4 + SPLITTER + STRING_DATA_PATTERN_5 + SPLITTER + STRING_DATA_PATTERN_6 + SPLITTER
-            + STRING_DATA_PATTERN_7 + SPLITTER + STRING_DATA_PATTERN_8 + "))$";
+        "^((" + STRING_DATA_PATTERN_1 + ")|(" + STRING_DATA_PATTERN_2 + ")|(" + STRING_DATA_PATTERN_3 + ")|("
+            + STRING_DATA_PATTERN_4 + ")|(" + STRING_DATA_PATTERN_5 + ")|(" + STRING_DATA_PATTERN_6 + ")|("
+            + STRING_DATA_PATTERN_7 + ")|(" + STRING_DATA_PATTERN_8 + "))$";
     private static final Pattern DATA_PATTERN = Pattern.compile(STRING_DATA_FULL_PATTERN);
 
     public static Optional<LocalDate> parseDate(String dataStr) {
         LocalDate date = null;
         Matcher dataMatcher = DATA_PATTERN.matcher(dataStr);
         if (dataMatcher.find()) {
-            if (dataStr.matches("^(" + STRING_DATA_PATTERN_1 + SPLITTER + STRING_DATA_PATTERN_2 + ")$")) {
+            if (dataStr.matches("^(" + STRING_DATA_PATTERN_1 + ")|(" + STRING_DATA_PATTERN_2 + ")$")) {
                 String[] args = dataStr.split("-");
                 date = LocalDate.of(
                     Integer.parseInt(args[0]),
