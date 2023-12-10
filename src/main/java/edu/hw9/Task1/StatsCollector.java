23d41b8071d8011b2a0b45d8e291c6f2d64055e9
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2023-12-10 13:33:55 +0300

    task_1

---
+++ src/main/java/edu/hw9/Task1/StatsCollector.java
@@ -0,0 +1,21 @@
+package edu.hw9.Task1;
+
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class StatsCollector {
+    private final BlockingQueue<Metric> collector;
+    public StatsCollector(int collectorSize) {
+        collector = new LinkedBlockingDeque<>(collectorSize);
+    }
+
+    public void put(String metricName, double[] data) throws InterruptedException {
+        collector.put(new Metric(metricName, data));
+    }
+
+    public String collectStats() throws InterruptedException {
+        Metric metric = collector.take();
+        return metric.getName() + ": " + metric.sum() + " " + metric.avg() + " " + metric.max() + " " + metric.min();
+    }
+}
