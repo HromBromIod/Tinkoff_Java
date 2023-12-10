7098c156ca09e746e922ca5045884d1eeda70a66
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2023-12-11 01:40:30 +0300

    task_1_2_3

--- src/main/java/edu/hw9/Task3/Solver.java
+++ src/main/java/edu/hw9/Task3/Solver.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Stack;
 import java.util.concurrent.RecursiveTask;
 
+@SuppressWarnings("CyclomaticComplexity")
 public class Solver extends RecursiveTask<List<Cell>> {
     private final Stack<Cell> cellStack;
     private final Labyrinth labyrinth;
