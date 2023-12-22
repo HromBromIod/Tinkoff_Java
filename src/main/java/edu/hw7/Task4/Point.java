f1a60b131d64a47e50b97d940df3c2afb1fcfb86
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2023-12-22 17:54:51 +0300

    task_1_3_4

--- src/main/java/edu/hw7/Task4/Point.java
+++ src/main/java/edu/hw7/Task4/Point.java
@@ -1,11 +1,4 @@
 package edu.hw7.Task4;
 
-public class Point {
-    protected final double x;
-    protected final double y;
-
-    public Point(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
+public record Point(double x, double y) {
 }
