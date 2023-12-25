b1a14ab6d45b0a2fa95412788ff4c68909037a34
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2023-12-25 16:20:41 +0300

    task_4

--- src/main/java/edu/hw7/Task4/Task4.java
+++ src/main/java/edu/hw7/Task4/Task4.java
@@ -10,31 +10,31 @@ public class Task4 {
     }
 
     //Среднее время ускорения решения (1_000_000_000): 1.8718092751097823
-    //One Thread PI: 3.141631592; Погрешность: 3.8938410206856844E-5
+    //One Thread PI: 3.141212; Погрешность: 3.8065358979322284E-4
     //Multi Thread PI: 3.1416058085664234; Погрешность: 1.3154976630236348E-5
 
     //Среднее время ускорения решения (100_000_000): 1.547336522434407
     //One Thread PI: 3.14171124; Погрешность: 1.1858641020667804E-4
     //Multi Thread PI: 3.1414632656585306; Погрешность: 1.2938793126249237E-4
 
-    //Среднее время ускорения решения (10_000_000): 0.953596004543705
-    //One Thread PI: 3.1416276; Погрешность: 3.4946410206959655E-5
-    //Multi Thread PI: 3.1411734564693825; Погрешность: 4.191971204106082E-4
+    //Среднее время ускорения решения (10_000_000): 1.3151861139680816
+    //One Thread PI: 3.141286; Погрешность: 3.066535897930933E-4
+    //Multi Thread PI: 3.1419412; Погрешность: 3.4854641020709565E-4
 
     private static final long TEN_MILLIONS = 10_000_000;
 
     public static void main(String[] args) throws InterruptedException {
         double expected;
         var start = System.nanoTime();
-        expected = oneThreadPI(TEN_MILLIONS);
+        expected = oneThreadPI(TEN_MILLIONS * 1);
+        var second1 = System.nanoTime() - start;
         System.out.printf("One Thread PI: %s%n", expected);
         System.out.println(Math.abs(expected - Math.PI));
-        var second1 = System.nanoTime() - start;
         start = System.nanoTime();
-        expected = fourThreadsPI(TEN_MILLIONS);
+        expected = fourThreadsPI(TEN_MILLIONS * 1);
+        var second2 = System.nanoTime() - start;
         System.out.printf("Multi Thread PI: %s%n", expected);
         System.out.println(Math.abs(expected - Math.PI));
-        var second2 = System.nanoTime() - start;
         System.out.println((double) second1 / second2);
     }
 
@@ -53,18 +53,16 @@ public class Task4 {
         LongAdder totalCount = new LongAdder();
         LongAdder pointInCircleCount = new LongAdder();
         final int count_of_threads = 4;
-
         ThreadWithCounter[] threads = new ThreadWithCounter[count_of_threads];
 
         for (int i = 0; i < count_of_threads; ++i) {
-            threads[i] = new ThreadWithCounter();
-            threads[i] = new ThreadWithCounter(threads[i].threadPiRunnable(pointInCircleCount, countOfIterations / count_of_threads));
+            threads[i] = new ThreadWithCounter(countOfIterations / count_of_threads);
             threads[i].start();
+            totalCount.add(countOfIterations / count_of_threads);
         }
-
         for (ThreadWithCounter thread : threads) {
             thread.join();
-            totalCount.add(thread.getCounter());
+            pointInCircleCount.add(thread.getCounter());
         }
 
         return (4.0 * pointInCircleCount.longValue()) / totalCount.longValue();
--- src/main/java/edu/hw7/Task4/ThreadWithCounter.java
+++ src/main/java/edu/hw7/Task4/ThreadWithCounter.java
@@ -5,29 +5,25 @@ import java.util.concurrent.atomic.LongAdder;
 import static edu.hw7.Task4.Circle.isPointInCircle;
 
 public class ThreadWithCounter extends Thread {
-    private int counter = 0;
+    private long counter;
+    private final long countOfIterations;
 
-    public ThreadWithCounter() {
-        super();
+    public ThreadWithCounter(long countOfIterationsPerThread) {
+        counter = 0;
+        countOfIterations = countOfIterationsPerThread;
     }
 
-    public ThreadWithCounter(Runnable runnable) {
-        super(runnable);
-    }
-
-    public int getCounter() {
-        return counter;
-    }
-
-    public Runnable threadPiRunnable(LongAdder pointInCircleCount, long countOfIterationsPerThread) {
-        return () -> {
-            for (long i = 0; i < countOfIterationsPerThread; ++i) {
-                Point p = new Point(ThreadLocalRandom.current().nextDouble(), ThreadLocalRandom.current().nextDouble());
-                if (isPointInCircle(p)) {
-                    pointInCircleCount.increment();
-                }
+    @Override
+    public void run() {
+        for (long i = 0; i < countOfIterations; ++i) {
+            Point p = new Point(ThreadLocalRandom.current().nextDouble(), ThreadLocalRandom.current().nextDouble());
+            if (isPointInCircle(p)) {
                 counter += 1;
             }
-        };
+        }
+    }
+
+    public long getCounter() {
+        return counter;
     }
 }
